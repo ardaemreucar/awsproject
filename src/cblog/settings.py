a227e8634255766d4a437942dc23e8db93770e9b
Arda Emre Ucar <120659080+workaeu@users.noreply.github.com>
2023-04-02 16:07:45 +0300

    Update settings.py

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database-1', # database name in RDS is written here
+        'NAME': 'database1', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
         'HOST': 'database-1.cghc7dssqeii.us-east-1.rds.amazonaws.com',  # database endpoint is written here
